d860f1e725850bffaea45b9170b58b9be7a6cdd0
skinnyfads <skinnyfads@proton.me>
2026-02-22 19:50:48 +0700

    feat: make the splash screen to be in dark mode

--- app/src/main/java/com/example/skybase/MainActivity.kt
+++ app/src/main/java/com/example/skybase/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
@@ -39,6 +40,7 @@ import com.example.skybase.ui.theme.ThemeMode
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
