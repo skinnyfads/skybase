cb8d291f5c43a3c9c7ded6660b3028787a2a92a2
skinnyfads <skinnyfads@proton.me>
2026-04-15 07:47:57 +0700

    chore(release): bump app version to 1.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
         minSdk = 24
         targetSdk = 36
         versionCode = 1
-        versionName = "1.2.0"
+        versionName = "1.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "JM_NEWS_BASE_URL", properties.getProperty("JM_NEWS_BASE_URL") ?: "\"\"")
